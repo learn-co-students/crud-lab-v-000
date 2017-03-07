ce8724fd140293203131647cc97ce54b7951ac0e
github username <github email address>
2017-03-07 09:24:29 -0500

    Add partial solution

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
-  render(){
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+
+  render() {
     return(
       <div>
-      </div>
-    )
+        <li>
+         {this.props.restaurant.text}
+          <button onClick={() => this.handleOnClick()}> X </button>
+          <ReviewInput
+            store={this.props.store}
+            restaurantId={this.props.restaurant.id} />
+        </li>
+       </div>
+    );
   }
-}
+};
 
-export default Restaurant
+export default Restaurant;
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Restaurant from './restaurant';
 
 class Restaurants extends Component {
-  render(){
+
+  render() {
+    const restaurants =
+    this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} store={this.props.store} />
+    })
+    console.log(restaurants)
+
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
-    )
+    );
   }
-}
-
+};
 export default Restaurants;
