4e03abd637accfc1686590b8c63f0b5631819e68
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-30 17:31:02 +0000

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
+    const restaurantList = this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} id={restaurant.id} store={this.props.store}/>
+    })
     return(
       <ul>
         Restaurants Component
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,13 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = { restaurants: [] }, action) {
+  switch(action.type) {
+    case 'ADD_RESTAURANT':
+      const newRestaurant = Object.assign({}, action.name, { id: cuidFn() } )
+      console.log(newRestaurant)
+      return {...state, restaurants: state.restaurants.concat(newRestaurant) }
+    default:
+      return state
+  }
 }
