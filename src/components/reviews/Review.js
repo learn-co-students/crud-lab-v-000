435597d1853aae264a0c4add84953eab9449bde7
Idayat <github email address>
2017-05-20 12:24:20 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+  handleOnClick(){
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.id
+    })
+  }
   render() {
     return (
+      <div>
       <li>
+        {this.props.review.text}
+        <button onClick={() => this.handleOnClick()}>X</button><br/>
       </li>
+      </div>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,10 +3,21 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+
+    const associatedReviews = this.props.store.getState().reviews.filter(review => {
+      return review.restaurantId === this.props.restaurantId
+    })
+
+    const reviews = associatedReviews.map((review, index) => {
+      return <Review review={review} key={index} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
 };
