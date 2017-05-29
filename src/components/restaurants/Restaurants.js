922145450cee17a7885868862b71ee488d5d86ff
walwoodr <walwoodr@gmail.com>
2017-05-29 17:36:42 -0500

    Some words

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -4,13 +4,13 @@ import Restaurant from './Restaurants';
 class Restaurants extends Component {
   render() {
     const {store} = this.props;
-    const restaurants = store.getState().restaurants.map((item, index) => {
+    const allRestaurants = store.getState().restaurants.map((item, index) => {
       return <Restaurant store={store} key={item.id} restaurant={item} />
     });
-    
+
     return(
       <ul>
-        {restaurants}
+        {allRestaurants}
       </ul>
     );
   }
