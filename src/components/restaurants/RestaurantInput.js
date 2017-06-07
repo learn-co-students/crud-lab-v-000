4a36988934443f8d35aa4209405e0f6bb08f16d8
github username <jilustrisimo@users.noreply.github.com>
2017-06-07 13:58:20 -0400

    building RestaurantInput, passing 3 tests

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: '',
+    }
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={ e => this.handleSubmit(e) }>
+          <input type='text' onChange={ e => this.handleChange(e) } value={ this.state.text } />
+          <input type='submit' />
+        </form>
       </div>
     );
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: {
+        text: this.state.text
+      }
+    })
+    this.setState({
+      text: ''
+    })
   }
 };
 
