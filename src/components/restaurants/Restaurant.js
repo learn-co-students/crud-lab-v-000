b365636b74f42333baa83b1fb6487f7050082df1
github username <jilustrisimo@users.noreply.github.com>
2017-06-07 14:09:32 -0400

    built Restaurant

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Restaurant extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <li>
+        { this.props.restaurant.text }
+        <button onClick={ () => this.handleClick() }>Delete</button>
+      </li>
     );
+  }
+
+  handleClick() {
+    this.props.store.dispatch({
+      type: 'DELETE RESTAURANT',
+      id: this.props.restaurant.id
+    })
   }
 };
 
