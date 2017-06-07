774ea7f02ff69c496b6fd6fc0233d72eeb4c1456
github username <jilustrisimo@users.noreply.github.com>
2017-06-07 14:09:19 -0400

    building reducer, built first case

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,20 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
 
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const newRestaurant = Object.assign({}, 
+      action.restaurant, {
+        id: cuidFn()
+      })
+      return { restaurants: state.restaurants.concat(newRestaurant) }
+
+
+    default:
+    return state
+  }
 }
