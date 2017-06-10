a38cfd42428f145f554c504174f6d098b97ecb14
Nathan <admin@bigrocketgames.com>
2017-06-10 16:49:21 -0700

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class Restaurant extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+
   render() {
     return (
       <div>
+        <li>{this.props.restaurant.text}</li>
+        <button onClick={() => this.handleOnClick()}>Delete</button>
       </div>
     );
   }
