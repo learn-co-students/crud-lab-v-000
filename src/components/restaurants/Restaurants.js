2a1b61b2d017adfc5f716712abeea24574ec0b8b
Michael Ries <michael.ries@gmail.com>
2017-07-09 12:44:35 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+  filterReviews = (restaurant) => {
+    const state = this.props.store.getState();
+    if (state.reviews) {
+      return state.reviews.filter(review => 
+        review.restaurantId === restaurant.id
+      )
+    }
+    return [];
+  }
   render() {
+    const restaurants = this.props.store.getState()
+                  .restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} 
+                        store={this.props.store}
+                        reviews={this.filterReviews(restaurant)} />
+    })
+
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
+import Reviews from '../reviews/Reviews'
 
 class ReviewInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW', 
+      review: {
+        restaurantId: this.props.restaurantId,
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: '',
+    });
+  }
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input type="text" onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+        <Reviews store={this.props.store} 
+                restaurantId={this.props.restaurantId}/>
       </div>
     );
   }
