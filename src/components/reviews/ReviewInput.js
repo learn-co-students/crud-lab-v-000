29a40614b4e1f56b707b258f3cbfad0bca949977
Hui Jiang <huijiang@Huis-MBP.home>
2017-07-27 11:01:57 -0400

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
+import Reviews from '../reviews/Reviews'
 
 class ReviewInput extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = { text: ''};
+  };
+
+  handleOnChange(event) {
+  	this.setState({text: event.target.value});
+  }
+
+  handleOnSubmit(event) {
+  	event.preventDefault();
+  	this.props.store.dispatch({
+  		type: 'ADD_REVIEW',
+  		review: {
+  			text: this.state.text,
+  			restaurantId: this.props.restaurantId
+
+  		}
+  	});
+  	this.setState({text: ''});
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        	<label> enter review for this restaurant</label>
+        	<input type="text" value={this.state.text} onChange={(event) => this.handleOnChange(event)} />
+        	<input type="submit" />
+        </form>
+        <Reviews store={this.props.store} restaurantId={this.props.restaurantId}/>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,15 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const filteredReviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId);
+
+    const reviews = filteredReviews.map((review, idx) => {
+        return <Review key={idx} review={review} store={this.props.store} />
+      });
+
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
