34b3fd6c1843e80aa382efb5a73707257d412a11
github username <github email address>
2017-07-30 18:06:41 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+
+  handleClick = event => this.props.store.dispatch({type: "DELETE_REVIEW", id: this.props.review.id })
   render() {
     return (
       <li>
+        {this.props.review.text}
+        <button type='button' onClick={this.handleClick}>Delete</button>
       </li>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,29 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+let reviewCount = 0
+
+export default function manageRestaurants(state = {restaurants: [], reviews: []}, action) {
+
+  switch (action.type){
+    case 'ADD_RESTAURANT':
+    var restaurant = {text: action.name, id: cuidFn()}
+    return Object.assign({}, state, {restaurants: state.restaurants.concat(restaurant)})
+
+    case 'DELETE_RESTAURANT':
+      return Object.assign({}, state, {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)})
+
+      case "ADD_REVIEW":
+        reviewCount++
+        let review = {text: action.text, restaurantId: action.id, id: reviewCount}
+        return Object.assign({}, state, {reviews: state.reviews.concat(review)})
+
+      case 'DELETE_REVIEW':
+      return Object.assign({}, state, {reviews: state.reviews.filter(review => review.id !== action.id)})
+
+
+    default:
+      return state
+  }
 
 }
