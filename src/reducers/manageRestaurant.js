da294cc17ba36586f14c87ef5f8e0270b8271f7d
Sergio Mendoza <smendoza787@gmail.com>
2017-08-01 21:11:33 -0700

    setup react and redux to add and delete restaurant resources

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,17 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = { ...action.restaurant, id: cuid() };
+      return { restaurants: state.restaurants.concat(restaurant) };
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { restaurants };
+    default:
+      return state;
+  }
 }
