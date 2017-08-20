b9c5948513fbced402603d21effe986cca2cfcbb
Ivan Pilot <ivanpilot@gmail.com>
2017-08-20 19:33:54 +0200

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
+  constructor(){
+    super();
+    this.state = {
+      reviews: []
+    }
+
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  handleDelete(){
+    this.props.store.dispatch({
+      type:'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    })
+  }
+
   render() {
     return (
       <div>
+        <li>
+          Description: {this.props.restaurant.text}
+          <button onClick={this.handleDelete}>DELETE</button>
+          <ReviewInput store={this.props.store} restaurantId={this.props.restaurant.id}/>
+        </li>
       </div>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleTyping = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: {
+        text: this.state.text,
+        restaurantId: this.props.restaurantId
+      }
+    });
+
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Review: </label>
+          <input type="text" onChange={this.handleTyping} value={this.state.text}/>
+          <input type="submit" />
+        </form>
+        <Reviews store={this.props.store} restaurantId={this.props.restaurantId}/>
       </div>
     );
   }
