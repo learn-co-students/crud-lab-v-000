4f1cf33c5ef22cad5020ca0a2355d8bda57acb57
github username <github email address>
2017-09-03 18:13:48 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 
+// class Review extends Component {
+  
+//   handleOnClick() {
+//     this.props.store.dispatch({
+//       type: 'DELETE_REVIEW',
+//       id: this.props.review.id
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <li>
+//           {this.props.review.text}
+//         </li>
+//         <button onClick={this.handleOnClick}> </button>
+//       </div>
+//     );
+//   }
+
+
+// };
+
 class Review extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.id
+    });
+  }
+
   render() {
     return (
-      <li>
-      </li>
+      <div>
+        <li>
+          {this.props.review.text}
+        </li>
+        <button onClick={() => this.handleOnClick()}> X </button>
+      </div>
     );
   }
+
 };
+
 
 export default Review;
