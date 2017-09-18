6d2881dd1286a20c5d9c7c6117cda12524b99680
Luisa Scavo <luisascavo@gmail.com>
2017-09-18 10:57:12 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
+
+  handleDeleteClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <li>
+      {this.props.restaurant.text}
+      <button onClick={this.handleDeleteClick}>Delete</button>
+      <ReviewInput restaurant={this.props.restaurant} restaurantId={this.props.restaurant.id} />
+      </li>
     );
   }
 };
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: {
+        text: this.state.text,
+        restaurantId: this.props.restaurantId,
+      }
+    });
+    this.setState({
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.text} onChange={this.handleChange}/>
+          <button type='submit'></button>
+        </form>
+        < Reviews restaurantId = {this.props.restaurantId} store= {this.props.store}/>
       </div>
     );
   }
