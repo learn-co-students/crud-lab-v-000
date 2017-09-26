4f0b11e2da61ac64a1366d31e95d729f64a56079
mcclains2003 <mcclains2003@gmail.com>
2017-09-26 13:38:15 -0500

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+
   render() {
     return (
       <div>
+        <li>
+          {this.props.restaurant.text}
+          <button onClick={ this.handleOnClick }>Delete</button>
+          <div>
+            Review: 
+            <ReviewInput restaurantId={this.props.restaurant.id} 
+                         store={this.props.store} />
+          </div>
+        </li>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,47 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+    const { store, restaurantId } = this.props;
+    const associatedReviews = store.getState().reviews.filter(review => review.restaurantId === restaurantId);
+    const reviews = associatedReviews.map((review, index) => {
+      return <Review store={store} key={index} review={review} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
+
 };
 
 export default Reviews;
+
+
+
+// import React, { Component } from 'react';
+// import Review from './Review';
+
+// class Reviews extends Component {
+//   render() {
+//     const resReviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId)
+//     const reviews = resReviews.map((review,index)=>{
+//       return (
+//         <Review review={review} key={index} store={this.props.store} />
+//       )
+//     })
+
+//     return (
+//       <ul>
+//         {reviews}
+//       </ul>
+//     );
+//   }
+// };
+
+// export default Reviews;
