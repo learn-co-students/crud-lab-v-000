3514cb26b5350d623a36cc372602d2e75f093f21
Bob Habig <rfhab2@gmail.com>
2017-10-01 13:28:30 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const restaurants = this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index}
+                         store={this.props.store}
+                         id={restaurant.id}
+                         restaurant={restaurant} />
+    })
+
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
