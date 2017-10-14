e188a2fb591fa67b306b1c76116ca2af087d2007
bshap93 <bshap93@gmail.com>
2017-10-14 14:39:35 -0600

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput.js'
 
 class Restaurant extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    })
+  }
+
   render() {
     return (
       <div>
+        <li>{this.props.restaurant.text}
+        <button onClick={this.handleOnClick}>Delete</button>
+        <ReviewInput restaurantId={this.props.restaurant.id} store={this.props.store} />
+        </li>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,23 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {
+  restaurants: [], reviews: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = Object.assign({}, action.restaurant, { id: cuid() })
+      return { restaurants: state.restaurants.concat(restaurant) }
+    case 'DELETE_RESTAURANT':
+      return { restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
+    case 'ADD_REVIEW':
+      const review = Object.assign({}, action.review, { id: cuidFn() })
+      return Object.assign({}, state, {
+        reviews: state.reviews.concat(review)
+      })
+    case 'DELETE_REVIEW':
+      return { reviews: state.reviews.filter(review => review.id !== action.id) }
+    default:
+      return state;
+  }
 }
