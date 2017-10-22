25b4c17ada079d757c13d2a76aeef41507eba1b3
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-22 16:19:17 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,16 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+
+    const reviews = this.props.store.getState().reviews.map((review, index) => {
+      if (review.restaurantId == this.props.restaurantId) {
+        return <Review review={review} key={index} store={this.props.store} restaurantId={this.props.restaurantId}/>
+      }
+    });
+
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
