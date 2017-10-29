507a705bb9f4525da5236d28e8d339e55bdf9ce9
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-10-29 17:52:41 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(){
+    super();
+    this.state = {text : ''};
+  }
+
+  handleOnChange = (event) => {
+    this.setState({text: event.target.value});
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type:'ADD_RESTAURANT',
+      restaurant: {text: this.state.text},
+    });
+    this.setState({text:''});
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <label>Restaurant name: </label>
+          <input type="text" value={this.state.text}
+            onChange={this.handleOnChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,38 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state={
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type){
+    case 'ADD_RESTAURANT':
+      let restaurant = Object.assign({}, action.restaurant, {id:cuid()});
+      return {
+        restaurants: state.restaurants.concat(restaurant),
+        reviews: state.reviews
+      };
+    case 'DELETE_RESTAURANT':
+      return {
+        restaurants: state.restaurants.filter((restaurant)=>{
+          return restaurant.id !== action.id;
+        }),
+        reviews: state.reviews
+      };
+    case 'ADD_REVIEW':
+      let review = Object.assign({}, action.review, {id:cuid()});
+      return {
+        restaurants: state.restaurants,
+        reviews: state.reviews.concat(review),
+      };
+    case 'DELETE_REVIEW':
+      return {
+        restaurants: state.restaurants,
+        reviews: state.reviews.filter((review)=>{
+          return review.id !== action.id;
+        })
+      };
+    default:
+      return state;
+  }
 }
