de348cdbbbb0178d39f838b45e9e9e60e32eee66
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-11-02 17:18:56 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const restaurants = this.props.store.getState().restaurants.map((shop) =>{
+      return <Restaurant restaurant={shop} store={this.props.store} />
+    })
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,17 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {restaurants: [],}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      let id = cuid();
+      let restaurant = Object.assign({}, action.restaurant, {id: id});
+      return {restaurants: state.restaurants.concat(restaurant)};
+    case 'DELETE_RESTAURANT':
+      let restaurants = state.restaurants.filter(shop => shop.id !== action.id)
+      return {restaurants: restaurants}  
+    default:
+      return state;
+  }
 
 }
