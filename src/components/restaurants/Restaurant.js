5428401d685a271e62777b83e9dfeb25a219734a
github username <github email address>
2017-11-03 10:22:17 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput'
 
 class Restaurant extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    })
+  }
   render() {
+    const {text, id} = this.props.restaurant
     return (
-      <div>
-      </div>
+      <li>
+        {text}
+        <button onClick={this.handleOnClick} />
+        <ReviewInput restaurantId={id} store={this.props.store} />
+      </li>
     );
   }
 };
