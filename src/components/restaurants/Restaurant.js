da4fa7c95360692d6b259faf3931693bfa394456
Allen Anderson <dande313@gmail.com>
2017-11-08 13:33:03 +0000

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class Restaurant extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <li>
+        {text}
+        <button onClick={this.handleOnClick}>Delete</button>
+        <ReviewInput
+          store={this.props.store}
+          restaurantId={id} />
+      </li>
     );
   }
 };
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Restaurants extends Component {
   render() {
+    const restaurants = this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} store={this.props.store} />
+    });
     return(
+
       <ul>
-        Restaurants Component
+        { restaurants }
       </ul>
     );
   }
