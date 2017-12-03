e476a2f4ddaf32a55fc88c22df04010c7ac82f9d
laramontana <larabrightside@gmail.com>
2017-12-04 00:20:16 +0300

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: this.state.text
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.text} onChange={this.handleChange} />
+          <button type="submit">Add Restaurant</button>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,18 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type) {
+    case "ADD_RESTAURANT":
+      return {...state, restaurants: state.restaurants.concat(  {restaurant: action.restaurant, id: cuid()}  )   }
+
+
+    default:
+      return state
+
+  }
 
 }
