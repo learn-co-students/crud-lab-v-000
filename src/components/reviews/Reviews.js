a762efa06bdd97397075ccb072a8ffff049050e4
Malind Kowalski <malind.nikulski@gmail.com>
2017-12-07 15:32:55 -0700

    finally finished redux CRUD lab

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,10 +3,18 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const {store, restaurantId} = this.props
+    const restaurantReviews = store.getState().reviews.filter(review => review.restaurantId === restaurantId)
+    const reviews = restaurantReviews.map((review, index) => {
+      return <Review review={review} key={index} store={store}/>
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
 };
