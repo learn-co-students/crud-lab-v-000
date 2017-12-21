addc32d3178a11687f3a2bcb3f73ba0c9dd94fcf
github username <github email address>
2017-12-21 15:42:01 -0600

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,47 @@
+
 import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: '',
+    }
+  };
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: this.state.text,
+      restaurantId: this.props.restaurantId,
+    });
+
+    this.setState({
+      text: '',
+    })
+  }
+
+  handleOnChange(event){
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={event => this.handleOnSubmit(event)}>
+          <input type="text"
+                 value={this.state.text}
+                 onChange={event => this.handleOnChange(event)}
+          />
+          <input type="submit" />
+        </form>
+
+        <Reviews restaurantId={this.props.restaurantId} store={this.props.store} />
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,39 @@
+let id =0;
 
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state={
+    restaurants: [],
+    reviews: [],
+}, action) {
+    switch(action.type){
+        case 'ADD_RESTAURANT':
+          id = cuid();
+          const restaurant = Object.assign({}, action.restaurant, {id: id});
+          return {restaurants: state.restaurants.concat(restaurant)};
 
-}
+        case 'DELETE_RESTAURANT':
+          const restaurants = state.restaurants.filter(r => r.id !== action.id);
+          return { restaurants };
+
+        case 'ADD_REVIEW':
+          return {
+            restaurants: state.restaurants,
+            reviews: state.reviews.concat({
+              text: action.review,
+              id: cuid(),
+              restaurantId: action.restaurantId,
+            })};
+
+        case 'DELETE_REVIEW':
+          const reviews = state.reviews.filter(r => r.id !== action.id);
+          return {
+              restaurants: state.restaurants,
+              reviews: reviews,
+           };
+
+        default:
+          return state;
+    };
+};
