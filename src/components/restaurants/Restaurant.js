dcbb4e87b753e64a4867f72718d58a1ec9da3123
github username <github email address>
2017-12-24 13:22:51 -0600

    finished restaurant section

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Restaurant extends Component {
+
+  handleDelete = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+
   render() {
     return (
-      <div>
-      </div>
+        <li>
+          {this.props.restaurant.text}
+          <button onClick={this.handleDelete}>Delete</button>
+        </li>
     );
   }
 };
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,21 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = Object.assign({}, action.restaurant, {id: cuidFn()})
+      const restaurant_state = Object.assign({}, state, { restaurants: state.restaurants.concat(restaurant) });
+      //console.log(restaurant_state)
+      return restaurant_state
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return { restaurants }
+    default:
+      return state
+  }
 
 }
