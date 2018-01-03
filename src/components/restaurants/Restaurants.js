0441c169c2f109c10c920651b981b080a94a336a
Mel Chang <mel510@users.noreply.github.com>
2018-01-03 12:48:37 -0800

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
+    const restaurants = this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant restaurant={restaurant} key={index} store={this.props} />
+    })
+
     return(
       <ul>
-        Restaurants Component
+        { restaurants }
       </ul>
     );
   }
-};
+}
 
 export default Restaurants;
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+    const { store, restaurantId } = this.props
+    const restaurantReviews = store.getState().reviews.filter(review => review.restaurantId === restaurantId)
+    const reviews = restaurantReviews.map((review, index) => {
+      return <Review review={review} key={index} store={store} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
-};
+}
 
 export default Reviews;
