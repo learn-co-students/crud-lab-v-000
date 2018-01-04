58d0dbd09ecd89187f1c9ab069697c3fe8aa145a
Bruce Molina <bruce.molina.81@gmail.com>
2018-01-04 06:04:16 +0000

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.review_id
+    })
+  }
+
   render() {
     return (
-      <li>
-      </li>
+      <div>
+        <li>
+          {this.props.text}
+          <button onClick={this.handleOnClick}/>
+        </li>
+      </div>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.review_id
+    })
+  }
+
   render() {
+   const specific_reviews = this.props.store.getState().reviews.filter((review)=> {return review.restaurantId === this.props.id});
+
+   const reviews = specific_reviews.map((review, index) => {return <Review review={review} text={review.text} id={review.review_id} key={index}  store={this.props.store}  />  })
+
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
-    );
+    )
   }
-};
+}
 
 export default Reviews;
