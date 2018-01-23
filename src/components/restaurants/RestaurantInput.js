4c07c8e4bdd320631d2cb1dcd6d3e7d6ff4bfaf6
jake <jtb1137@gmail.com>
2018-01-23 12:17:50 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: {
+        text: this.state.text,
+      }
+    });
+    this.setState({
+      text: '',
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Restaurant Name: </label>
+          <input type='text' onChange={(event) => this.handleChange(event)} value={this.state.text} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+    const { store, restaurantId } = this.props;
+    const assocReviews = store.getState().reviews.filter(review => {
+      return review.restaurantId === restaurantId
+    });
+    const reviews = assocReviews.map((review, index) => {
+      return <Review 
+      key={index}
+      store={store}
+      review={review}
+      />
+    })
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
