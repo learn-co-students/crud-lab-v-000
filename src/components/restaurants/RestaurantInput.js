65232cd7c366098fd0c9900324812b3b7360d3a0
dbfarms <matt.hunger@gmail.com>
2018-01-27 16:36:36 +0000

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,102 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: {
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: ''
+    });
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <input 
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
 export default RestaurantInput;
+
+
+/*
+
+import React, { Component } from 'react';
+
+class RestaurantInput extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: '',
+    };
+  }
+  
+  
+  handleOnChange(event){
+    this.setState({
+      text: event.target.value,
+    })
+  }
+  
+  handleOnSubmit(event){
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurants: {
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: '',
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <p>
+          <label>Add a Restaurant</label>
+          <input 
+            type="text" 
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit"/>
+          </p>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default RestaurantInput;
+
+*/
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+  
   render() {
+    const { store } = this.props;
+    const restaurants = store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} store={store} />
+    });
+    
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
 };
 
 export default Restaurants;
+
+
+/*
+
+import React, { Component } from 'react';
+import Restaurant from './Restaurant'
+
+class Restaurants extends Component {
+  
+  render() {
+    const { store } = this.props; 
+    const restaurants = store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} store={store} />
+    });
+    
+    return(
+      <ul>
+        {restaurants}
+      </ul>
+    )
+  }
+};
+
+export default Restaurants;
+
+*/
