198048f157c207f83da9e1715a3b609f1f36b945
Sophia Hubeny <shhubeny@gmail.com>
2018-01-29 21:13:42 +0000

    4 review tests passing

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import Reviews from './Reviews';
 
+
 class ReviewInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text:'',
+    }
+  }
+
+  handleOnChange(event){
+    this.setState({
+      text: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)}/>
+        </form>
       </div>
     );
   }
