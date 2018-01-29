950023c2a9a117270fd9daecd0a1cc52e11edf0a
Sophia Hubeny <shhubeny@gmail.com>
2018-01-29 20:35:44 +0000

    2 tests down

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text:'',
+    }
+  }
+
+  handleOnSubmit(event){
+    this.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: this.state,
+    });
+  }
+
+  handleKeyDown(event){
+    this.setState({
+      text: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            onKeyDown={this.handleKeyDown}/>
+          <input
+            type="submit"/>
+        </form>
       </div>
     );
   }
