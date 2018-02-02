c9370c407c49635872385b515712d9c7571e8aa9
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-02 06:13:37 +0000

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -3,26 +3,33 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {
-    restaurants: [], 
-    reviews: [], 
+  restaurants: [],
+  reviews: [],
 }, action) {
   switch (action.type) {
+    
     case 'ADD_RESTAURANT':
-      const restaurant = Object.assign({}, action.restaurant, { id: cuidFn() });
-      return { restaurants: state.restaurants.concat(restaurant) }
+      const restaurant = { text: action.restaurant.text, id: cuidFn() };
+      return Object.assign({}, state, { 
+        restaurants: state.restaurants.concat(restaurant),
+      });
+
     case 'DELETE_RESTAURANT':
       const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
-      return  { restaurants }
+      return Object.assign({}, state, { restaurants });
+
     case 'ADD_REVIEW':
-      const review = Object.assign({}, action.review, { id: cuidFn() }); 
-      debugger;
-      return  { reviews: state.reviews.concat(review) }
-    //   return Object.assign({}, state, { reviews: state.reviews.concat(review) })
+      const review = Object.assign({}, action.review, { id: cuidFn() });
+      return Object.assign({}, state, {
+        reviews: state.reviews.concat(review),
+      });
+
     case 'DELETE_REVIEW':
       const reviews = state.reviews.filter(review => review.id !== action.id);
-      return  { reviews }
-    default:
-      return state;
-  }
+      return Object.assign({}, state, { reviews });
 
-}
+    default: 
+      return state;
+
+  }
+};
