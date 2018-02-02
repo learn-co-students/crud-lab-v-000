e32a0716205f755c2ab12945d7d5b7c9734f6370
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-02 03:04:57 +0000

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
+import Reviews from './Reviews'; 
+import Restaurant from '../restaurants/Restaurant'
 
 class ReviewInput extends Component {
+  constructor() {
+    super();
+      this.state = {
+       text: '',
+    };
+  } 
+  
+  handleChange(event) {
+  this.setState({
+    text: event.target.value
+  });
+};
+
+handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW', 
+      review: {
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: '',
+    });
+  }
+  
   render() {
-    return (
+    debugger;
+    return ( 
       <div>
-        Review Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <p>
+            <label>add review</label>
+            <input type="text" 
+            onChange={(event) => this.handleChange(event)}/>
+          </p>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    
+    const reviews = this.props.store.getState().reviews.map((review, index) => {
+      return <Review review={review} key={index} id={review.id} store={this.props.store}/>
+    });
+    
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
