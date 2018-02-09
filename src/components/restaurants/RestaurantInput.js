b2f6c64bdaeeb40ead44cd760ae12077c4b1e6b3
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-08 18:45:59 -0800

    all restaurant input tests pass

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleKeyDown = (event) => {
+    this.setState({
+      text: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({type: 'ADD_RESTAURANT', text: this.state.text})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input onChange={(event) => this.handleKeyDown(event)} value={this.state.text} />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,14 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = { restaurants: []}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = {text: action.text, id: cuidFn()}
+      return { restaurants: [...state.restaurants, restaurant] }
+    case 'DELETE_RESTAURANT':
+      return 'something';
+    default:
+      return state;
+  }
 }
