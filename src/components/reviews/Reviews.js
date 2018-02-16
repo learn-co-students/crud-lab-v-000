3c1719d19e3281014e61175a6e4b537f4c094a78
emhopkins <epoletto@lstreetc.com>
2018-02-15 20:15:07 -0600

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Review from './Review';
-
+ 
 class Reviews extends Component {
+ 
   render() {
+ 
+    const reviews = this.props.store.getState().reviews.filter(e => e.restaurantId === this.props.restaurantId).map((review, index) => {
+      return <Review key={index} review={review} store={this.props.store} /> 
+    });
+ 
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
 };
-
+ 
 export default Reviews;
