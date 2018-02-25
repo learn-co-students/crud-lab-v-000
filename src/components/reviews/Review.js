d3552141531bc51263af1be085e8c7b39493d413
Soumya Veer <veer.soumya@gmail.com>
2018-02-25 23:48:42 +0000

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,12 +1,24 @@
+
 import React, { Component } from 'react';
 
 class Review extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.id
+    });
+  }
+
   render() {
     return (
       <li>
+        {this.props.review.text}
+        <button onClick={() => this.handleOnClick()}> X </button>
       </li>
     );
   }
 };
 
 export default Review;
+
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,23 @@
+
 import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const restaurantsReviews = this.props.store.getState().reviews.filter(review =>
+      review.restaurantId === this.props.restaurantId);
+
+    const reviews = restaurantsReviews.map((review, index) => {
+      return <Review store={this.props.store} key={index} review={review} />
+    });
+
     return (
       <ul>
-        Reviews
+        { reviews }
       </ul>
     );
   }
 };
 
 export default Reviews;
+
