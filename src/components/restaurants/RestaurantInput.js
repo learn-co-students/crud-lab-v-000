772cbb2bd0f80e432158da1cd0f0dd86aacf810a
DashaDaria <dashadariallc@gmail.com>
2018-02-27 20:50:53 +0900

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import cuid from 'cuid';
 
 class RestaurantInput extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      text: ''
+    }
+  };
+
+  handleChange(event){
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: {
+        text: this.state.text,
+        id: cuid()
+      }
+    })
+    this.setState({
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <p>
+            <label>Restaurant Name </label>
+            <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)}/>
+          </p>
+            <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,24 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {restaurants: [],reviews: []}, action) {
+  switch (action.type){
 
+    case 'ADD_RESTAURANT':
+      return Object.assign({}, state, {restaurants: state.restaurants.concat(action.restaurant)});
+
+    case 'ADD_REVIEW':
+      return Object.assign({}, state, {reviews: state.reviews.concat(action. review)});
+
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return {restaurants}
+
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id)
+      return {reviews}
+
+    default:
+      return state
+  }
 }
