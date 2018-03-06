15c340a4a6da747657d46daae549166d2f1315eb
ackerm44 <ackerm44@gmail.com>
2018-03-06 14:52:18 -0500

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput'
 
 class Restaurant extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    })
+  }
+
+
   render() {
     return (
       <div>
+        <li>{this.props.restaurant.text}</li>
+        <button onClick={this.handleOnClick}>Delete</button>
+        <ReviewInput restaurantId={this.props.restaurant.id} store={this.props.store} />
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const filteredReviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId )
+    const reviews = filteredReviews.map((review, index) => {
+      return <Review review={review} key={index} store={this.props.store} />
+    })
+
     return (
       <ul>
-        Reviews
+      {reviews}
       </ul>
     );
   }
