47961f8d82465ee40c98de30390973f1b51266cf
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-03-20 17:27:53 +0000

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import cuid from 'cuid';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+ 
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+  
+  handleSubmit(event) {
+  event.preventDefault();
+  this.props.store.dispatch({
+    type: 'ADD_RESTAURANT',
+    restaurant: {
+      text: this.state.text,
+      id: cuid()
+    }
+  });
+}
+ 
   render() {
-    return (
+    return(
       <div>
-        Restaurant Input
-      </div>
-    );
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <label>
+          Restaruant Name:
+         <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+     </div>
+   );
   }
 };
 
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import Reviews from './Reviews';
+import cuid from 'cuid';
 
 class ReviewInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+ 
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+  
+  handleSubmit(event) {
+  event.preventDefault();
+  this.props.store.dispatch({
+    type: 'ADD_REVIEW',
+    review: {
+      text: this.state.text,
+      id: cuid()
+    }
+  });
+}
+ 
   render() {
-    return (
+    return(
       <div>
-        Review Input
-      </div>
-    );
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <label>
+          Review:
+         <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+     </div>
+   );
   }
 };
 
