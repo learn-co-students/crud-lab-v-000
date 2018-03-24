c95df428235520aca6d1a7ebeef725b34f4ebee9
CLackrone <charisaanne@gmail.com>
2018-03-23 22:48:18 -0400

    in progress

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,20 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {restaurants: [], reviews: [] }, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT': 
+    const restaurant = {...action.restaurant, id: cuid()}
+      return {restaurants: [...state.restaurants, restaurant]}
+    case 'DELETE_RESTAURANT': 
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return {restaurants}
+
+    case 'ADD_REVIEW':
+      const review = {...action.review, id: cuid()}
+      return {reviews: [...state.reviews, review]}
+    default: 
+      return state
+  }
 
 }
