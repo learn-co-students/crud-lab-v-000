00acffb3ec70671a9211c11897a06d156d22e0d2
CodeDad <github email address>
2018-03-23 21:32:46 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,10 +3,17 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const linkedReviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId);
+    
+    const reviews = linkedReviews.map((review, index)=>{
+      return <Review key={index} store={this.props.store} review={review} />
+    })
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
 };
