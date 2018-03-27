73790881aa5dbb126328d001f471a0680c10bf8c
streamedLine <dovidlwtn@gmail.com>
2018-03-26 22:26:34 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Restaurant extends Component {
+  
+  handleClick = (e)=> {
+  	this.props.store.dispatch({type: 'DELETE_RESTAURANT', id: this.props.restaurant.id})
+  }
+
   render() {
     return (
       <div>
+	    <li>
+	    	name: {this.props.restaurant.text} <button onClick={this.handleClick}>x</button>
+	    </li>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,16 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {restaurants: []}, action) {
+	switch (action.type) {
+		case 'ADD_RESTAURANT':
+			const restaurant = {text: action.text, id: cuidFn()}
+			return {restaurants: state.restaurants.concat(restaurant)}
 
+		case 'DELETE_RESTAURANT':
+		  return {restaurants: state.restaurants.filter(restaurant=> restaurant.id != action.id)}
+
+		default:
+			return state
+	}
 }
