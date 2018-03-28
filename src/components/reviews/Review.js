d5834eed06ad6f4656ceb3ad1a9087fe16fedaab
kyle-poretto <kyleporetto@gmail.com>
2018-03-28 14:48:18 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+
+  deleteReview(event){
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.id
+    })
+  }
+
   render() {
+    let text = this.props.review.text
     return (
       <li>
+          {text}
+          <button onClick={event => this.deleteReview(event)}> Delete Review </button>
       </li>
     );
   }
