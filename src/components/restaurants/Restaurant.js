9177b392eb12c265503dc3f8fcc6617ded497401
David Towster <djt4xe@virginia.edu>
2018-04-30 03:19:54 -0700

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
+
+  handleClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT', 
+      id: this.props.restaurant.id
+    });
+  }
+
   render() {
     return (
       <div>
+        <li>
+          {this.props.restaurant.text}
+          <button onClick={this.handleClick}/>
+        </li>
+        <ReviewInput 
+          store={this.props.store} 
+          restaurantId={this.props.restaurant.id}
+        />
       </div>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: this.state.text,
+      restaurantId: this.props.restaurantId
+    });
+    this.setState({
+      text: ""
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          <input type="submit" />
+        </form>
+        <Reviews store={this.props.store} restaurantId={this.props.restaurantId}/>
       </div>
     );
   }
