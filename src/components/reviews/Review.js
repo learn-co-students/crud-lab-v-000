06ff11dde4dff160eccc12a307940bbb40cf2940
github username <github email address>
2018-05-30 15:08:36 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Review extends Component {
+
+  deleteReview() {
+    this.props.store.dispatch({
+      type: "DELETE_REVIEW",
+      id: this.props.review.id
+    })
+  }
+
   render() {
     return (
       <li>
+        {this.props.review.text}
+        <button onClick={() => this.deleteReview()}> X </button>
       </li>
-    );
+    )
   }
-};
+}
 
-export default Review;
+export default Review
