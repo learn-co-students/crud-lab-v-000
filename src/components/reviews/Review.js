ca821b5e453b2606e7b0978fdde86097f8859435
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-11 11:20:15 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleDeleteReview = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      id: this.props.review.id,
+    })
+    debugger;
+  }
+
   render() {
     return (
       <li>
+        {this.props.review.text}
+        <button onClick={() => this.handleDeleteReview()}/>
       </li>
     );
   }
