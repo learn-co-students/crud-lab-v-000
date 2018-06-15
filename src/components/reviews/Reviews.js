cd3a170cf431318d5e0dfc5c9090baba203b3e8c
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-15 11:59:13 -0400

    2 tests left to go

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,30 +3,30 @@ import Review from './Review';
 
 class Reviews extends Component {
 
+
+// TN: helpful for seeing internal state
   clicky = () => {
     let st = this.props.store.getState()
     console.log(st)
+
+    const reviews = this.props.store.getState().reviews
+    // .filter(r => r.restaurantID === this.props.restaurantID)
+    console.log(reviews)
+    console.log('resyaurant id: ', this.props.restaurantId)
+
   }
 
   render() {
 
-    // const reviews = this.props.store.getState().reviews.filter(review => review.restaurantID === this.props.restaurantID).map((review, index) => {
-    //   return (<Review store={this.props.store} key={index} review={review}/>)})
-
-
-    // console.log("reviews: ", reviews)
-    // .getState().reviews.filter(review => review.restaurantID === this.props.restaurantID).map((review, index) => {
-    //   return (<Review store={this.props.store} key={index} review={review}/>)})
-  //
-  //   map((restaurant, index) => {
-  //   return (<Restaurant restaurant={restaurant} key={index} id={restaurant.id} store={this.props.store}/>)
-  // })
-
-    // this.props.store.getState().restaurants.
+// TN: Big thing that messed me up for a while: I KEPT WRITING restaurantID instead of restaurantId!!
+// BIG WASTE OF TIME FOR THIS MISTAKE!
+    const reviews = this.props.store.getState().reviews.filter(r => r.restaurantId === this.props.restaurantId).map((review, index) => {
+       return (<Review store={this.props.store} key={index} review={review}/>)})
 
     return (
       <ul>
         Review console button: <button onClick={(event)=>this.clicky(event)} />
+        {reviews}
       </ul>
     );
   }
