724f28ce288e95647915fae54dccb442dc8a38ab
BuffaloRob <peckham.rob@gmail.com>
2018-07-22 18:07:12 -0400

    restaurant portion working

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Restaurant extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+  
   render() {
     return (
       <div>
+        <li>
+          {this.props.restaurant.text}
+          <button onClick={this.handleOnClick}>Delete</button>
+        </li>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,22 @@
 
+
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+let id = 0;
 
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      id++;
+      const restaurant = Object.assign({}, action.restaurant, {id: id});
+      return { restaurants: state.restaurants.concat(restaurant) }
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return {restaurants}
+    default:
+      return state;
+  }
 }
