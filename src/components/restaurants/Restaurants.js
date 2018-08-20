e7b1e3fd6fa6bdf02163a6d32b8a566b8d359a1d
catalanowebdevelopment <catalano.work@gmail.com>
2018-08-20 13:32:03 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Restaurants extends Component {
-  render() {
-    return(
-      <ul>
-        Restaurants Component
-      </ul>
-    );
-  }
-};
-
-export default Restaurants;
+    
+}
