88b6d00a752ebf0443c3093f332e41bf0ebb5ce4
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-03 19:49:25 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -11,7 +11,11 @@ export default function manageRestaurants(state = {
       return { ...state, restaurants: [...state.restaurants, restaurant] }
 
     case 'DELETE_RESTAURANT':
-      return { ...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) }
+      return {
+        ...state,
+        restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id),
+        reviews: state.reviews.filter(review => review.restaurantId !== action.id)
+      }
 
     case 'ADD_REVIEW':
       const review = { text: action.review.text, id: cuidFn(), restaurantId: action.review.restaurantId }
