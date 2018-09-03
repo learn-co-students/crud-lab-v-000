e5664fd5ae13b47cea44f44076b0e8908b2a2bbd
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-03 11:28:09 -0700

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const restaurantreviews = reviews.filter(review => review.restaurantId === restaurantId)
+    const reviewList = restaurantreviews.map(review => {
+      return (
+        <Review 
+          key={review.id} 
+          review={review}
+          deleteReview={deleteReview}
+        />
+      )
+    })
+
     return (
       <ul>
-        Reviews
+        { reviewList }
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,17 +2,30 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state = {restaurants: [],}, action) {
+export default function manageRestaurants(
+    state = {
+        restaurants: [], 
+        reviews: [],
+    }, action) {
     switch (action.type) {
         case 'ADD_RESTAURANT':
             const restaurant = { id: cuidFn(), text: action.text }
-            return { restaurants: state.restaurants.concat(restaurant) }
+            return {...state, restaurants: [...state.restaurants, restaurant] }
         
         case 'DELETE_RESTAURANT':
             const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
-            return {restaurants}
+            return {...state, restaurants}
+
+        case 'ADD_REVIEW':
+            const review = { id: cuidFn(), restaurantId: action.review.restaurantId, text: action.review.text }
+            return {...state, reviews: [...state.reviews, review] }
+        
+        case 'DELETE_REVIEW':
+            const reviews = state.reviews.filter(review => review.id !== action.id);
+            return {...state, reviews}
+
         default:
-          return state;
+          return state
     }
 }
     
