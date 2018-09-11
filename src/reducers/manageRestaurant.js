27f0b393e0798ae34c0aadf2f575c47f7855e288
Donna <donnamcla@gmail.com>
2018-09-10 21:15:06 -0400

    restaurants container in wrong place, and added actions for reviews

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -17,6 +17,16 @@ export default function manageRestaurants(state = {
     case "DELETE_RESTAURANT":
       const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
       return { ...state, restaurants }
+      
+    case "ADD_REVIEW":
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
+
+    case "DELETE_REVIEW":
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews}
 
     default:
       return state;
