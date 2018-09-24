96241d5418beee6fdd3b1f1245a64b08e70d3c65
Kiveo <mavericktesini@gmail.com>
2018-09-24 16:39:16 -0700

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    // decouple
+    // const restaurants = this.props;
+    // replace index with id later
+    const renderRestaurants = () => { this.props.restaurants.map(restaurant => {<Restaurant key={restaurant.id} restaurant={restaurant} /> }) }
+    // const renderRestaurants = () => {return [] }
+    
     return(
       <ul>
-        Restaurants Component
+        {renderRestaurants()}
       </ul>
     );
   }
