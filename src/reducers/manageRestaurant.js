fe809c8f1e3eda9f921bff3f45f4c0c8b6a79a6a
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-09-25 08:48:09 -0700

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,28 +1,37 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state={restraunts:[], reviews:[]}, action) {
-  switch(action.type){
-    case "ADD_RESTAURANT":
-      const restaurant = {id: cuidFn(), text: action.text}
-      return{...state, restaurants:[...state.restaurants, restaurant]}
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type) {
 
-    case "DELETE_RESTAURANT":
-      const restaurants = state.restaurants.filter(r => r.id !== action.id)
-        return {restaurants}
+    case 'ADD_RESTAURANT':
+
+      const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
+      }
+
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { ...state, restaurants}
 
     case 'ADD_REVIEW':
 
       const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
-        return { ...state,
-          reviews: [...state.reviews, review]
-        }
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
 
-      case 'DELETE_REVIEW':
-        const reviews = state.reviews.filter(review => review.id !== action.id);
-        return {...state, reviews }
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
 
-      default:
-        return state;
+    default:
+      return state;
+
   }
-}
+};
