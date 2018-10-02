13efee1cdf56074baf289bc414b30d188345e118
kanndide <kanndide@gmail.com>
2018-10-02 18:48:54 +0300

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+	renderRestaurants = () => this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant} deleteRestaurant={this.props.deleteRestaurant} />)
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
 };
 
-export default Restaurants;
+const mapStateToProps = ({ restaurants }) => ({ restaurants })
+
+const mapDispatchToProps = dispatch => ({
+  deleteRestaurant: id => dispatch({ type: "DELETE_RESTAURANT", id})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Restaurants);
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+  	const { reviews, restaurantId, deleteReview } = this.props
+  	const matchedReviews = reviews.filter(review => review.restaurantId === restaurantId)
+  	const renderReviews = matchedReviews.map(review => {
+  		return <Review key={review.id} review={review} deleteReview={deleteReview} />
+  	})
+    debugger
     return (
       <ul>
-        Reviews
+        {renderReviews}
       </ul>
     );
   }
 };
 
-export default Reviews;
+
+export default Reviews
