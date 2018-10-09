320d35d6d4fa2ab73fc55bb7763af774a69f28f5
badlychadly <chadmontoya21@yahoo.com>
2018-10-09 11:41:51 -0700

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleKeyDown = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+
+
   render() {
     return (
       <div>
-        Review Input
+        <form >
+          <input type="text" value={this.state.text} onChange={this.handleKeyDown}/>
+          <input type="submit" value="Add Review"/>
+        </form>
       </div>
     );
   }
