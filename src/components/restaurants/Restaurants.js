9acb3e5571db532f5f9745d94bfa0c8226de2b2f
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-14 22:45:21 -0400

    Implement Restaurants component

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+  renderRestaurants = () => this.props.restaurants.map(restaurant =>
+    <Restaurant key={restaurant.id} delete={id => this.props.deleteRestaurant(id)} restaurant={restaurant} />
+  )
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
 };
 
-export default Restaurants;
+const mapStateToProps = state => {
+  return {
+    restaurants: state.restaurants
+  }
+}
+
+export default connect(mapStateToProps)(Restaurants);
