c89b09f6794a85fd5afe1a0302f53665c0952cdf
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-14 22:31:22 -0400

    Begin to implement reducer

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,21 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants
+(
+    state = {
+        restaurants: [],
+        reviews: []
+    }, 
+    action
+) 
+{
+    switch(action.type){
+        case "ADD_RESTAURANT":
+            const restaurant = {
+                id: cuid(),
+                text: action.text
+            }
+            return {...state, }
+    }
 }
