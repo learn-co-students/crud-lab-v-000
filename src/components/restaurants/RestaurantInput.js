b6d84ac0f443fd55978d3071c5100f245d772233
coryjbergquist <cory.j.bergquist@gmail.com>
2018-10-21 17:39:11 -0700

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  };
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault;
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ""
+    });
+  }
+
   render() {
     return (
       <div>
+      <form onSubmit={(event) => this.handleSubmit(event)} >
+        <input type="text" onChange={(event) => this.handleChange(event)} />
+      </form>
         Restaurant Input
       </div>
     );
