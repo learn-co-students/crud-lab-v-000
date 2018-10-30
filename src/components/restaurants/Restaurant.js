1d3296435992720c36695a2b1aaa52044190da69
kreopelle <kreopelle@gmail.com>
2018-10-30 10:14:38 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -5,12 +5,12 @@ class Restaurant extends Component {
 
 
   render() {
-    // const { restaurant, deleteRestaurant } = this.props;
+    const { restaurant } = this.props;
 
     return (
       <div>
         <li>
-          {this.props.restaurant.text}
+          {restaurant.text}
           <button onClick={() => this.props.deleteRestaurant(this.props.restaurant.id)}> X </button>
           <ReviewsContainer restaurant={this.props.restaurant}/>
         </li>
