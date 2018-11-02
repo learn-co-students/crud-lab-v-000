9f02ec79b62e28545221e989105e061c4563f182
Kim Gonzales <Kimgo589@gmail.com>
2018-11-02 12:01:08 -0400

    begin building the manageRestaurant reducer

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,22 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants( 
+  state = {
+    restaurants: [],
+    reviews: []
+  }, action) {
+  
+  switch ( action.type ) {
+    case "ADD_RESTAURANT":
+      return // code to add restaurant
+    case "DELETE_RESTAURANT":
+      return // code to delete restaurant
+    case "ADD_REVIEW":
+      return //code to add new review
+    case "DELETE_REVIEW":
+      return //delete review
+    default:
+      return state
+  }
 }
