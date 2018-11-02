b7cc062a96dd4385c6a27e7f82ab2df1cfbdbbc1
Dillorom <dabdulloeva@gmail.com>
2018-11-02 10:28:45 -0700

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 
 class RestaurantInput extends Component {
+  state = {
+    text: ""
+  }
+  handleSubmit = e => {
+    e.preventDefautl();
+    this.props.addRestaurnat(this.state)
+    this.setState({
+      name: ""
+    })
+    
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit} >
+        <label htmlFor="Restaurant"></label>
+          <input type="text" value={this.state.text} onChange={this.handleChange}></input>
+          <input type="submit">Submit</input>
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+
+const mapDispatchToProps = dispatch => ({
+  addRestaurant:  addRestaurant => dispatch({ type: 'ADD_RESTAURANT', restaurant})
+})
+export default connect(null, mapDispatchToProps)(RestaurantInput);
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+//import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
+  // state = {
+  //   restaurants: []
+  // }
 
   render() {
     return (
@@ -13,5 +17,11 @@ class RestaurantsContainer extends Component {
     )
   }
 }
+
+// const mapStateToProps = state => {
+//   return {
+//     restaurants: state.restaurants
+//   }
+// }
 
 export default RestaurantsContainer
