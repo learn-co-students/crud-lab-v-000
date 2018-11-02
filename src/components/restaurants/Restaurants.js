e54bd74ec1743e3f1777c29ba2145c72f8d7f794
Kim Gonzales <Kimgo589@gmail.com>
2018-11-02 16:18:24 -0400

    pass down restaurant id and text prop from restaurants to restaurant

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
+
+    const { restaurants } = this.props;
+
+    const restaurantsList = restaurants.map(restaurant => {
+      return (
+       <Restaurant key={restaurant.id} restaurant={restaurant}/>
+      )
+    })
+
+
     return(
       <ul>
-        Restaurants Component
+        {restaurantsList}
       </ul>
     );
   }
