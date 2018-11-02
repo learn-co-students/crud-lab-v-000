7e8c2578f9b1fb6e3f1ed71fae13a3ad58d89f40
Kim Gonzales <Kimgo589@gmail.com>
2018-11-02 12:36:07 -0400

    add handlers to restaurantinput form

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange(event){
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+
   render() {
     return (
       <div>
         Restaurant Input
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text" 
+            name="text"
+            value={this.state.text}
+            onChange={(e) => this.handleChange(e)} 
+          />
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
     );
   }
 };
+
 
 export default RestaurantInput;
