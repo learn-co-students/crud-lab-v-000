65cfff04471dcc9655c6ba9a9b3780ed19177b6e
Kim Gonzales <Kimgo589@gmail.com>
2018-11-05 14:28:51 -0500

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,43 +1,37 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants( 
-  state = {
-    restaurants: [],
-    reviews: []
-  }, action) {
-  
-  switch ( action.type ) {
-    case "ADD_RESTAURANT":
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type) {
+
+    case 'ADD_RESTAURANT':
+
       const restaurant = { text: action.text, id: cuidFn() };
-      return { ...state, restaurants: [...state.restaurants, restaurant ]}
-    case "DELETE_RESTAURANT":
-       return {restaurants: state.restaurants.filter( restaurant => restaurant.id !== action.id)};
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
+      }
+
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { ...state, restaurants}
+
+    case 'ADD_REVIEW':
+
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
+
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
+
     default:
-      return state
+      return state;
+
   }
-}
-
-
-
-// export default function manageRestaurants( 
-//   state = {
-//     restaurants: [],
-//     reviews: []
-//   }, action) {
-  
-//   switch ( action.type ) {
-//     case "ADD_RESTAURANT":
-//       debugger
-//       console.log("hit the reducer")
-//       return [...state].concat(action.restaurants)
-//     case "DELETE_RESTAURANT":
-//       return // code to delete restaurant
-//     case "ADD_REVIEW":
-//       return //code to add new review
-//     case "DELETE_REVIEW":
-//       return //delete review
-//     default:
-//       return state
-//   }
-// }
+};
