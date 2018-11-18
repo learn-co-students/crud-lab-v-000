3541325a5ee9e2d8d2e16a3a543f5cde2ac1ee36
Araba <araba.taylor20@gmail.com>
2018-11-18 12:04:50 -0500

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,21 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+let initialState = {
+  restaurants: []
 }
+
+export default function manageRestaurants(state = initialState, action) {
+  switch (action.type){
+    case 'ADD_RESTAURANT':
+    const restaurant = {
+      id: cuid(),
+      name: action.name
+    }
+
+  return {...state, restaurants:[...state.restaurants, restaurant]}
+
+  default:
+    return state;
+  }
+};
