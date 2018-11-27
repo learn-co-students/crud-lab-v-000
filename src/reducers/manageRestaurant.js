5a09c7e7b33471fbe302c08ad8e3b5740e112018
Su0414 <susanaik.com>
2018-11-27 10:54:40 -0800

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,22 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state={
+    restaurants: [],
+    reviews: [],
+  }
+    , action) {
+    switch (action.type) {
+
+        case 'ADD_RESTAURANT':
+    
+          const restaurant = { text: action.text, id: cuidFn() };
+          return {
+            ...state,
+            restaurants: [ ...state.restaurants, restaurant]
+          }
+        default:
+            return state;    
+    }
 
 }
