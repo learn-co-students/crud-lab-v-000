0eb6594c2eeda84d61fa395a0924d7107e6e766d
Breyanna Vass <breyannavass@gmail.com>
2019-01-31 21:57:45 -0500

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
+//import { eventNames } from 'cluster';
+
 
 class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addReview({ text: this.state.text, restaurantId: this.props.restaurantId });
+    this.setState({
+      text: '',
+    });
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input type='text' onChange={this.handleOnChange} value={this.state.text} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import manageRestaurant from './reducers/manageRestaurant'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(manageRestaurant)
+const store = createStore(manageRestaurant, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root')
+  document.getElementById('root')
 )
