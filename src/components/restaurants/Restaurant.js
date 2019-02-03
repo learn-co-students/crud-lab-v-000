f119a79941f66e911f0ae1e1182f4ea68ee55fce
vickicello <vicki.cello@gmail.com>
2019-02-03 18:53:58 -0500

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer'
-
+// responsible for each restaurant
+// contains the button to delete 
+// Users can delete restaurants
+// each restaurant has an id (cuid)
 class Restaurant extends Component {
 
 
   render() {
-    const { restaurant } = this.props;
+    const { restaurant, deleteRestaurant, addReview, reviews, deleteReview } = this.props;
 
     return (
       <div>
         <li>
           {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+          <button onClick={() => deleteRestaurant(restaurant.id)}> X </button>
+          <ReviewsContainer restaurant={restaurant} addReview={addReview} reviews={reviews} deleteReview={deleteReview}/>
         </li>
       </div>
     );
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  renderReviews = () => this.props.reviews.filter(review => review.restaurantId === this.props.restaurant.id).map(review => <Review key={review.id} review={review} deleteReview={this.props.deleteReview}/>)
+
   render() {
     return (
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
