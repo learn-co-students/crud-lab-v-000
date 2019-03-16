ba541d1575ef3eee957fa77d894fa095e5d40fe3
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-15 19:18:37 -0600

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+
+  renderRestaurants = () => {
+    return this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} deleteRestaurant={this.props.deleteRestaurant} restaurant={restaurant} />)
+  }
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  renderReviews = () => {
+    return this.props.reviews.map(review => <Review key={review.id} restaurantId={review.restaurantId} eleteReview={this.props.deleteReview} review={review} />)
+  }
+
   render() {
     return (
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
