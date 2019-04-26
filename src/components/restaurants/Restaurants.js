4724c4bb340c6f3741794911ac0deacdf0e0ef5e
freeWillee <will.ik.lee@gmail.com>
2019-04-25 20:26:46 -0600

    Finish restaurant components (add and delete)

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const restaurants = this.props.restaurants
+    const restaurantList = restaurants.map(restaurant => {
+      return (
+        <Restaurant key={restaurants.id} restaurant={restaurant} 
+        deleteRestaurant = {this.props.deleteRestaurant}
+        />
+      )
+    })
+
     return(
       <ul>
-        Restaurants Component
+        {restaurantList}    
       </ul>
     );
   }
