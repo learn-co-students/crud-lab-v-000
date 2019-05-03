01fc64469e6eb14e4ebb7edabd3f7b8a812ba89b
amitnyc83 <amit_nyc@live.com>
2019-05-03 14:17:37 -0400

    able to add and delete restaurants

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -5,13 +5,13 @@ class Restaurant extends Component {
 
 
   render() {
-    const { restaurant } = this.props;
 
+    const { restaurant } = this.props;
     return (
       <div>
         <li>
           {restaurant.text}
-          <button> X </button>
+          <button onClick={() => this.props.deleteRestaurant(this.props.restaurant.id)}>DELETE</button>
           <ReviewsContainer restaurant={restaurant}/>
         </li>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import manageRestaurant from './reducers/manageRestaurant'
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
