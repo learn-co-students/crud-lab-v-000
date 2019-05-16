71f4103109c525521d230d67269814ae4956c869
github username <github email address>
2019-05-16 17:50:15 -0400

    restaurant component/container

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+
   render() {
+    const restaurants = this.props.restaurants.map(restaurant => {
+      return (
+        <Restaurant
+          key={restaurant.id}
+          restaurant={restaurant}
+          delete={this.props.delete}
+        />
+      )
+    })
+
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,23 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  console.log(action)
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = {
+        id: cuid(),
+        text: action.text
+      }
+      return { restaurants: state.restaurants.concat(restaurant)};
+
+    case 'DELETE_RESTAURANT':
+      return { restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)};
+
+    default:
+      return state;
+  }
 
 }
