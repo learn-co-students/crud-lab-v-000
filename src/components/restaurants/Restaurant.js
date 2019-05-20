0e9bb7979afd50cc740daeb4e4bda71ff67062d0
yomi413 <yjescano@gmail.com>
2019-05-19 21:44:24 -0400

    Added code solution

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReviewsContainer from '../../containers/ReviewsContainer'
+import ReviewsContainer from '../../containers/ReviewsContainer';
 
 class Restaurant extends Component {
 
@@ -11,7 +11,7 @@ class Restaurant extends Component {
       <div>
         <li>
           {restaurant.text}
-          <button> X </button>
+          <button onClick={() => this.props.delete(restaurant.id)}> X </button>
           <ReviewsContainer restaurant={restaurant}/>
         </li>
       </div>
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+  renderRestaurants = () => {
+    return this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant} delete={this.props.delete} />)
+  }
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
