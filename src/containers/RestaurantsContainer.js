41c75bf27060894428c55fb54032090e03f50540
github username <Adeel.A.Mhd@gmail.com>
2019-05-30 17:54:28 -0400

    Done.

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react'
-import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Restaurants from '../components/restaurants/Restaurants'
+import React, { Component } from 'react';
+import RestaurantInput from '../components/restaurants/RestaurantInput';
+import Restaurants from '../components/restaurants/Restaurants';
+import { addRestaurant, deleteRestaurant } from '../actions/restaurants.js';
+import { connect } from 'react-redux';
+
 
 class RestaurantsContainer extends Component {
 
   render() {
+
+    const { restaurants, addRestaurant, deleteRestaurant } = this.props;
     return (
       <div>
-        <RestaurantInput />
-        <Restaurants />
+        <RestaurantInput addRestaurant={addRestaurant} />
+
+        <Restaurants
+          deleteRestaurant={deleteRestaurant}
+          restaurants={restaurants}
+        />
       </div>
     )
   }
 }
 
-export default RestaurantsContainer
+const mapStateToProps = state => {
+  return { restaurants: state.restaurants }
+}
+
+export default connect(mapStateToProps, { addRestaurant, deleteRestaurant }) (RestaurantsContainer);
