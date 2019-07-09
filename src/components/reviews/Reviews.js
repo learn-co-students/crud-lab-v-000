9cdfc0dcf3fe5e92f6dc294d03b0688956709103
github username <github email address>
2019-07-09 17:27:50 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  renderReviews = () => {
+	return this.props.reviews.map(review => {
+	if (review.restaurantId === this.props.restaurantId) {
+		return (<Review key={review.id} 
+			review={review} 
+			restaurantId={review.restaurantId}
+			delete={this.props.delete} />
+			)}
+		})
+	}
+
   render() {
-    return (
+  
+    return(
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
-};
+}
 
 export default Reviews;
