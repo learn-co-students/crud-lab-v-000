8e42f5b21469394ed8da34459385054a6d2f40c2
Mika88 <mikaspivak@gmail.com>
2019-07-16 15:18:25 -0700

    Done.

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
-
+import { connect } from 'react-redux'
 class ReviewsContainer extends Component {
 
   render() {
     return (
       <div>
-        <ReviewInput />
+        <ReviewInput addReview={this.props.addReview} restaurantId={this.props.restaurant.id}/>
         <Reviews />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps= ({reviews}) => ({reviews})
+const mapDispatchToProps = dispatch => ({
+  addReview: payload => dispatch({ type: "ADD_REVIEW", payload }),
+  delete: id => dispatch({ type: "DELETE_REVIEW", id})
+ })
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,28 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
-}
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []
+}, action) {
+    switch (action.type) {
+        
+        case 'ADD_RESTAURANT':
+            const restaurant = {
+            id: cuid(),
+            text: action.text
+            }
+            return { ...state, restaurants: [...state.restaurants, restaurant] }
+        case 'DELETE_RESTAURANT':
+           return {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
+        case 'ADD_REVIEW':
+           const review = {
+              id: cuid(),
+              text: action.payload.text,
+              restaurantId: action.payload.restaurantId
+           }
+           return { ...state, reviews: [...state.reviews, review]}
+        default:
+          return state;
+  }
+};
