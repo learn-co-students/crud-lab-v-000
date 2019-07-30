5cee42ad395798d438cd827e7a7154cd12c2dabf
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-30 15:51:22 -0400

    work on addRestaurant functionality

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+
   render() {
-    return(
+    const {restaurants} = this.props;
+    const restaurantList = restaurants.map(restaurant => {
+      return(
+        <Restaurant
+          key = {restaurant.id}
+          restaurant = {restaurant}
+        />
+      );
+    })
+
+    return (
       <ul>
-        Restaurants Component
+        {restaurantList}
       </ul>
-    );
+    )
   }
 };
 
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,20 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  console.log(action);
 
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = {
+        id: cuidFn(),
+        text: action.text
+      }
+      return {...state, restaurants: [...state.restaurants, restaurant]}
+
+    default:
+      return state;
+  }
 }
