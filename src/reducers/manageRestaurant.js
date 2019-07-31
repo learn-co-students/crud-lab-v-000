3530a3b4aeb88ae4469b755a798f04dae11be9c4
Rose Brennan <rose@grubhub.com>
2019-07-31 16:58:59 -0600

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -19,16 +19,16 @@ export default function manageRestaurants(state = {
     case 'DELETE_RESTAURANT':
       return {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
 
-    case 'ADD_REVIEW':
-
-      const review = {
-        // id: ,
-        text: action.text
-      }
-      // return { reviews: state.reviews.concat(review) }
-      return { ...state,
-        reviews: [...state.reviews, review]
-      }
+    // case 'ADD_REVIEW':
+    //
+    //   const review = {
+    //     // id: ,
+    //     text: action.text
+    //   }
+    //   // return { reviews: state.reviews.concat(review) }
+    //   return { ...state,
+    //     reviews: [...state.reviews, review]
+    //   }
 
     default:
       return state;
