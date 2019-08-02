9cf4bbb3638a48b5c95f3b03df5730e26249a3cb
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-08-02 16:05:23 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,15 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    let reviews;
+    let associatedReviews;
+    if (this.props.reviews) {
+      associatedReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+      reviews = associatedReviews.map(review => <Review key={review.id} review={review} deleteReview={this.props.deleteReview}/>)
+    }
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
     );
   }
