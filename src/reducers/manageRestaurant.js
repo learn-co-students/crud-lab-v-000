626703fd1737de2150d33da4faa9da2f903e508c
Rachel Wyatt <rw126@evansville.edu>
2019-08-09 07:04:20 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,8 +1,9 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state = {restaurants: []}, action) {
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []}, action) {
     switch (action.type) {
         case 'ADD_RESTAURANT':
           const restaurant = { text: action.text, id: cuidFn()}
@@ -10,7 +11,15 @@ export default function manageRestaurants(state = {restaurants: []}, action) {
         case 'DELETE_RESTAURANT':
             const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
             return { ...state, restaurants}
-        default:
-          return state;
-      }
+        case 'ADD_REVIEW':
+            const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+            return { ...state,
+                reviews: [...state.reviews, review]
+            }
+        case 'DELETE_REVIEW':
+            const reviews = state.reviews.filter(review => review.id !== action.id);
+            return {...state, reviews }
+                default:
+                return state;
+            }
 }
