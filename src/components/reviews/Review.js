87f8643b1094833062cfc0118954e003b5acf0bd
inkanimal <theatomproject@gmail.com>
2019-09-05 12:51:21 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 class Review extends Component {
 
-  render() {
-    const { review } = this.props
+  handleOnClick = () => {
+    this.props.deleteReview(this.props.review.id)
+  }
 
+  render() {
     return (
       <div>
         <li>
-          {review.text}
-        </li>
-        <button> X </button>
+          {this.props.review.text}
+          </li>
+          <button onClick={this.handleOnClick}> X </button>
       </div>
     );
   }
