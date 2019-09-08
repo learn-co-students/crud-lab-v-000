ef1a0868bfdb2a5cde26edc97cf64602e849ea60
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-08 10:38:39 -0400

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const store = createStore(manageRestaurant,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
