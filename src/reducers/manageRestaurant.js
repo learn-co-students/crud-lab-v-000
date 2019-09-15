1e8f038bae07ff72ec9a2aec67f05a969f9ed6a2
Jamie Giuliano <jamiepgiuliano@gmail.com>
2019-09-15 10:56:40 -0700

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -20,6 +20,19 @@ export default function manageRestaurants(state = {
     const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
     return { ...state, restaurants }
 
+    case "ADD_REVIEW":
+
+    const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() }
+    return {
+      ...state,
+      reviews: [...state.reviews, review]
+    }
+
+    case "DELETE_REVIEW":
+
+    const reviews = state.reviews.filter(review => review.id !== action.id)
+    return { ...state, reviews}
+
     default:
     return state;
   }
