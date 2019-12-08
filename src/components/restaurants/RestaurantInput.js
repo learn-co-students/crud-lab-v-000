1b79389900d743c5b46972aa9ce5af1091f46cab
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-12-08 14:44:57 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { addRestaurant } from '../../actions/restaurants';
-import RestaurantsContainer from '../../containers/RestaurantsContainer'
+//import RestaurantsContainer from '../../containers/RestaurantsContainer'
 
 import React, { Component } from 'react';
 
