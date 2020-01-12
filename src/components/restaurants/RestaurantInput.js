620f6e200d230772647fcb4f201f81a4ddb87b71
John Blank <veloblank@gmail.com>
2020-01-12 04:28:21 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RestaurantInput extends Component {
+  state = {
+    text: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({
+      text: ""
+    });
+  };
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Restaurant name"
+            value={this.state.text}
+            onChange={this.handleChange}
+          ></input>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
-};
+}
 
 export default RestaurantInput;
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,49 @@
+import cuid from "cuid";
+//export const cuidFn = cuid;
 
-import cuid from 'cuid';
-export const cuidFn = cuid;
-
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(
+  state = {
+    restaurants: [],
+    reviews: []
+  },
+  action
+) {
+  switch (action.type) {
+    case "ADD_RESTAURANT": {
+      const newRestaurant = {
+        id: cuid(),
+        text: action.payload
+      };
+      return {
+        ...state,
+        restaurants: [...state.restaurants, newRestaurant]
+      };
+    }
+    case "DELETE_RESTAURANT":
+      const restaurants = state.restaurants.filter(
+        restaurant => restaurant.id !== action.payload
+      );
+      return {
+        ...state,
+        restaurants: [...restaurants]
+      };
+    case "ADD_REVIEW":
+      const newReview = {
+        id: cuid(),
+        text: action.review.text,
+        restaurantId: action.review.restaurantId
+      };
+      return {
+        ...state,
+        reviews: [...state.reviews, newReview]
+      };
+    case "DELETE_REVIEW":
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {
+        ...state,
+        reviews: [...reviews]
+      };
+    default:
+      return state;
+  }
 }
