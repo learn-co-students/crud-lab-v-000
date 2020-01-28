c32a5f66952dc2fb0a676013ac825fe7e8c00552
github username <github email address>
2020-01-27 22:46:05 -0500

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    console.log('Submit')
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        Write a Review:<br />
+        <form onSubmit={this.handleOnSubmit}>
+          <input type='text' value={this.state.text} onChange={this.handleOnChange} />
+          <input type='submit' value='Submit Review'  />
+        </form>
       </div>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 
+
 class ReviewsContainer extends Component {
 
   render() {
