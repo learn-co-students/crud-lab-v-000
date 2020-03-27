2d61473ea83859d1b8e6b92e0918570310bc5e9b
dhintz <dhintz89@gmail.com>
2020-03-27 19:56:37 -0400

    Restaurant resource completed & tests passed

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,29 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(
+    state = {
+        restaurants: [],
+        reviews: []
+    },
+    action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            let newRestaurant = Object.assign({}, action.restaurant)
+            newRestaurant.id = cuid()
+            return {
+                ...state,
+                restaurants: state.restaurants.concat(newRestaurant)
+            }
 
+        case 'DELETE_RESTAURANT':
+            console.log(action)
+            return {
+                ...state,
+                restaurants: state.restaurants.filter(rest => rest.id !== action.restaurantID)
+            }
+
+        default:
+            return state;
+    }
 }
