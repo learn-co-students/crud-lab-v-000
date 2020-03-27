15b1e56f0218fe199c25eb9fe6ab1be3568a7f52
github username <github email address>
2020-03-27 13:50:08 -0700

    Done.

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 
+import { connect } from 'react-redux'
+
 class ReviewsContainer extends Component {
 
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurant={this.props.restaurant}  />
+        <Reviews reviews={this.props.reviews.filter(review => review.restaurantId === this.props.restaurant.id)} deleteReview={this.props.deleteReview} />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+
+const mapStateToProps = ({ reviews }) => ({ reviews })
+
+const mapDispatchToProps = dispatch => ({
+  addReview: review => dispatch({ type: "ADD_REVIEW", review }),
+  deleteReview: id => dispatch({ type: "DELETE_REVIEW", id})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,42 @@
-
+import { combineReducers } from "redux";
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+const manageRestaurants = combineReducers({
+  restaurants: restaurantsReducer,
+  reviews: reviewsReducer
+});
+
+export default manageRestaurants;
+
+function restaurantsReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = {
+        id: cuid(),
+        text: action.text
+      }
+      return [...state, restaurant]
+    case 'DELETE_RESTAURANT':
+      return state.filter(restaurant => restaurant.id !== action.id)
+    default:
+      return state;
+    }
+}
+
+function reviewsReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_REVIEW':
+      const review = {
+        id: cuid(),
+        text: action.review.text,
+        restaurantId: action.review.restaurantId
+      }
+      return [...state, review]
+    case 'DELETE_REVIEW':
+      return state.filter(review => action.id !== review.id)
+    default:
+      return state
+  }
 
 }
