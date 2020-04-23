241b65424b5934087242c99c747fd13559b62180
Richard Cho <richardcho100@gmail.com>
2020-04-23 16:11:55 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
+import Restaurant from './Restaurant'
+
 class Restaurants extends Component {
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant} deleteRestaurant={this.props.deleteRestaurant} />)}
       </ul>
     );
   }
 };
+
+Restaurants.defaultProps = {
+  restaurants: []
+}
 
 export default Restaurants;
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,34 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []
+}, action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const newRestaurant = {
+                id: cuidFn(),
+                text: action.text
+            }
+            return {...state, restaurants: [...state.restaurants, newRestaurant] }
+
+        case 'DELETE_RESTAURANT':
+            return {...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
+
+        case 'ADD_REVIEW':
+            const newReview = {
+                text: action.review.text,
+                restaurantId: action.review.restaurantId,
+                id: cuidFn()
+            }
+            return {...state, reviews: [...state.reviews, newReview] }
+
+        case 'DELETE_REVIEW':
+            return {...state, reviews: state.reviews.filter(review => review.id !== action.id)}
+
+        default:
+            return state
+    }
 
 }
