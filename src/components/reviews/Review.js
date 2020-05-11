8e0e81fe8c471faca969ad355c5c69652569bb82
Rebecca Tong <rtong@buffalo.edu>
2020-05-11 18:06:12 -0400

    Done.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -6,6 +6,11 @@ class Review extends Component {
       text: this.props.review.text
     }
 
+handleChange = event => {
+  this.setState({
+    text: event.target.value
+  })
+}
   render() {
     const { review } = this.props
 
@@ -13,11 +18,10 @@ class Review extends Component {
     return (
       <div>
         <li>
-
           {review.text}
         </li>
         <button onClick={() => this.props.delete(review.id)}> X </button>
-        <input type='text'></input>
+        <input type='text' onChange={this.handleChange} value={this.state.text}></input>
         <button onClick={() => this.props.update({id: review.id, text: this.state.text})}> Update </button>
       </div>
     );
