e3235f7fe4a576ee6b711c030481272b06eba8d0
github username <github email address>
2020-06-09 13:53:49 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class RestaurantInput extends Component {
+
+  constructor() {
+    super(),
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({text: ''})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input type='text'
+            value={this.state.text}
+            onChange={(event) => this.handleChange(event)}/>
+          <input type='submit'/>
+        </form>
       </div>
     );
   }
