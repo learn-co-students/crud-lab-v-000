4529dddc8ec34f10342b7fab2100c140b6922992
sdcro <sdcrouse91@gmail.com>
2020-08-26 19:17:35 -0700

    Done.

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux';
 
 class RestaurantsContainer extends Component {
 
   render() {
     return (
       <div>
-        <RestaurantInput />
+        <RestaurantInput createRestaurant={this.props.createRestaurant} />
         <Restaurants />
       </div>
     )
   }
 }
 
-export default RestaurantsContainer
+const mapStateToProps = ({ restaurants }) => ({ restaurants });
+// Note: That's an ES6 shortcut for state => ({ restaurants: state.restaurants }).
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createRestaurant: text => dispatch({ type: 'CREATE_RESTAURANT', text })
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer);
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,20 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(
+  state = { restaurants: [] },
+  action
+) {
+  switch(action.type) {
+    case 'CREATE_RESTAURANT':
+      return {
+        ...state,
+        restaurants: [
+          ...state.restaurants,
+          { text: action.text, id: cuid() }
+        ]
+      };
 
+    default: return state;
+  }
 }
