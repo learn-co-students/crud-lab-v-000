b7d5318f72a5bac94e186f43f3d7d2bf95e597a3
scottms724 <scottms724@gmail.com>
2020-11-02 14:00:26 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,10 @@ class Reviews extends Component {
   render() {
     return (
       <ul>
-        Reviews
+        <h4>Reviews:</h4>
+        {this.props.reviews.map(review =>
+          <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
+        )}
       </ul>
     );
   }
