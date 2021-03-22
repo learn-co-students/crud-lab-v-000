6d1018f1f1bc9077bcc9436c50507ae08f2e9a95
RonSala <ron.sala@gmail.com>
2021-03-22 19:29:28 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -4,7 +4,7 @@ import Restaurant from './Restaurant';
 class Restaurants extends Component {
   render() {
     
-    const restaurants = this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={...restaurant}/>)
+    const restaurants = this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant}/>)
 
     return(
       <ul>
