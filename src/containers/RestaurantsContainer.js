17ebc7751600d09c05ae5dc2c6b91f9d7d7ed225
tscotce <tatyana.celovsky@gmail.com>
2021-04-02 11:46:20 -0400

    Done.

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RestaurantInput from '../components/restaurants/RestaurantInput';
 import Restaurants from '../components/restaurants/Restaurants';
 
 class RestaurantsContainer extends Component {
 
   render() {
+    const { restaurants } = this.props;
+
     return (
       <div>
-        <RestaurantInput />
-        <Restaurants />
+        <RestaurantInput addRestaurant={this.props.addRestaurant}/>
+        <Restaurants restaurants={restaurants}/>
       </div>
     )
   }
 }
 
-export default RestaurantsContainer;
+const mapStateToProps = ({ restaurants }) => ({ restaurants })
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: text => dispatch({ type: "ADD_RESTAURANT", text }),
+    delete: id => dispatch({
+      type: 'DELETE_RESTAURANT', payload: id
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer);
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,20 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = { restaurants: [] }, action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+    
+          const restaurant = {
+            id: cuid(),
+            text: action.text
+          }
+          return { restaurant: state.restaurants.concat(restaurant) };
+    
+        case 'DELETE_RESTAURANT':
+            
+          return {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.payload)}
+    
+        default:
+          return state;
+    }
 }
