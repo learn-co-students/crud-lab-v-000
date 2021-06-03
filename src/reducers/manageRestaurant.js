a9bbd0488a01678793ab55e3889d1d94fb96aa57
Paul Fell <swellpf@gmail.com>
2021-06-02 21:28:51 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -16,15 +16,17 @@ export default function manageRestaurants(state = {
             return {...state, restaurants}
 
         case 'ADD_REVIEW':
-
-
+            const review = {text: action.review.text, restaurantId: action.review.restaurantId, id: cuid()};
+            return {
+                ...state,
+                reviews: [...state.reviews, review]}
 
         case 'DELETE REVIEW':
-
-        
+            const reviews = state.reviews.filter(review => review.id !== review.id)
+            return {...state, reviews}
 
             default:
                 return state;
       }
 
-}
+};
