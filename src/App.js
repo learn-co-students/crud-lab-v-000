5692d41c1ce3cf7e2fab0d151e647068a5cccaef
github username <github email address>
2021-06-09 22:10:45 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import RestaurantsContainer from './containers/RestaurantsContainer';
-
 class App extends Component {
   render() {
     return (
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import RestaurantInput from '../components/restaurants/RestaurantInput';
 import Restaurants from '../components/restaurants/Restaurants';
+import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
 
   render() {
+    debugger
     return (
       <div>
         <RestaurantInput />
@@ -14,4 +16,7 @@ class RestaurantsContainer extends Component {
   }
 }
 
-export default RestaurantsContainer;
+
+export default connect()(RestaurantsContainer);
+// You will then want to create a Restaurants component that renders a list of restaurants, 
+//and a Restaurant component that is responsible for each individual restaurant.
