4cb546c7e0d4f2f9409c8b2e8847da4172f65476
emanao <emanao@me.com>
2021-06-10 22:48:41 -0400

    Add a restaurant with text and id.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -6,8 +6,9 @@ export default function manageRestaurants(state={
 }, action) {
     switch(action.type){
         case "ADD_RESTAURANT":
-            //  debugger;
-            return {...state, restaurants:[...state.restaurants,action.restaurant]}
+            //   debugger;
+            return {...state, 
+                restaurants:[...state.restaurants,{...action.restaurant, id:cuid()}]}
         default:
             return state;
     }
