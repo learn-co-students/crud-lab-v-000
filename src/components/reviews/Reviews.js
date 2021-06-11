d01faf420b9f54b83c045eb1b25e67e45ffca938
emanao <emanao@me.com>
2021-06-11 07:05:05 -0400

    Update the state store after submitting the form.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,7 @@ class Reviews extends Component {
   render() {
     return (
       <ul>
-        Reviews
+        {this.props.reviews.map(review=><Review key={review.id} review={review}/>)}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -8,17 +8,21 @@ export default function manageRestaurants(state={
         case "ADD_RESTAURANT":
             //   debugger;
             return {...state, 
+                reviews:[...state.reviews],
                 restaurants:[...state.restaurants,{...action.restaurant, id:cuid()}]}
         case "DELETE_RESTAURANT":
             // debugger;
             return {...state, 
+                reviews:[...state.reviews],
                 restaurants: state.restaurants.filter(restaurant=>restaurant.id!==action.id)}
         case "ADD_REVIEW":
             // debugger;
-            return {...state, 
+            return {...state,
+                restaurants:[...state.restaurants], 
                  reviews:[...state.reviews,{...action.review, id:cuid()}]}
         case "DELETE_REVIEW":
             return {...state, 
+                restaurants:[...state.restaurants], 
                 reviews:[...state.review,{...action.review, id:cuid()}]}
         
         default:
