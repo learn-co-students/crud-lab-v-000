5a8a01a8fc5a8947961f2711e7b226948a5a765f
emanao <emanao@me.com>
2021-06-10 21:30:21 -0400

    Display form and handle input changes

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
-  render() {
-    return (
-      <div>
-        Restaurant Input
-      </div>
-    );
-  }
+    state={
+        text:''
+    }
+
+    handleChange = (ev)=>{
+        this.setState({
+            text: ev.target.value
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <input 
+                    type="text"
+                    name="text"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <input type="submit" value="submit"/>
+            </div>
+        );
+    }
 };
 
 export default RestaurantInput;
