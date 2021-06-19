6cb7e72e52b0def64dbd1953d3cb9b5a260a8e83
Ian Gutwinski <iangutwinski@gmail.com>
2021-06-19 16:11:14 -0400

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+  
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+  
+  handleOnSumbit = event => {
+    event.preventDefault();
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId});
+    this.setState({
+      text: '',
+    });
+  }
+  
   render() {
     return (
       <div>
         Review Input
+        <form onSubmit={this.handleOnSumbit}>
+          <input type="text" value={this.state.text} onChange={this.handleOnChange}/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
